27fd0b123576050f1a2d05788ec59cb89c27c86a
Jeroen Bosch <jeroon.bosch@gmail.com>
2022-01-29 12:38:30 +0200

    folder change

---
+++ Assets/Racoon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Racoon : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    //private InputAction jumpAction;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        //jumpAction = playerInput.actions["Jump"];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        float horizontal = input.x;
+        float vertical = input.y;
+
+        transform.position = new Vector2(transform.position.x + horizontal * 0.01f, transform.position.y + vertical* 0.01f);
+    }
+}
