efa91b2659fae9f1fec65200dd00c886fa6a9777
highearthorbit <toni.saarela00@gmail.com>
2022-01-29 14:09:42 +0200

    some changes

--- Assets/Scripts/Racoon.cs
+++ Assets/Scripts/Racoon.cs
@@ -7,22 +7,26 @@ public class Racoon : MonoBehaviour
 {
     private PlayerInput playerInput;
     private InputAction moveAction;
-    //private InputAction jumpAction;
+    private InputAction jumpAction;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
-        //jumpAction = playerInput.actions["Jump"];
+        jumpAction = playerInput.actions["Jump"];
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector2 jump = jumpAction.ReadValue<Vector2>();
         float horizontal = input.x;
         float vertical = input.y;
 
-        transform.position = new Vector2(transform.position.x + horizontal * 0.01f, transform.position.y + vertical* 0.01f);
+        transform.position = new Vector2(transform.position.x + horizontal * 0.05f, transform.position.y + vertical* 0.05f);
+        
+        
+        
     }
 }
