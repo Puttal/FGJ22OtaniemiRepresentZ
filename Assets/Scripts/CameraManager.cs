db8d641af4a828ade0f4410280d8f30467335073
Jeroen Bosch <jeroon.bosch@gmail.com>
2022-01-29 17:29:57 +0200

    sort by Y axis, remove debug spam

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -37,7 +37,7 @@ public class CameraManager : Singleton<CameraManager>
             distance -= 20f;
             distance = Mathf.Max(distance, 0f); //Cannot be below 0
 
-            Debug.Log(distance);
+            //Debug.Log(distance);
             _camera.fieldOfView = Mathf.Clamp(baseFov + (distanceFovMod * distance), 125f, 160f);
     
         } else if (racoons.Count > 2) {
