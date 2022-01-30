05c9bc5bc180e892a1adee7d63eb67b2eef30298
Jeroen Bosch <jeroon.bosch@gmail.com>
2022-01-30 15:04:31 +0200

    something that looks like a game

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -20,14 +20,14 @@ public class CameraManager : Singleton<CameraManager>
     private void FollowRacoons() {
         if (GameMaster.Instance.Racoons().Count == 1) {
             Vector3 pos = GameMaster.Instance.Racoons()[0].transform.position;
-            transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+            transform.position = new Vector3(pos.x, pos.y + 5f, transform.position.z);
 
         } else if (GameMaster.Instance.Racoons().Count == 2) {
             Vector3 pos1 = GameMaster.Instance.Racoons()[0].transform.position;
             Vector3 pos2 = GameMaster.Instance.Racoons()[1].transform.position;
 
             Vector3 halfWayVector = (pos1 + pos2) / 2;
-            transform.position = new Vector3(halfWayVector.x, halfWayVector.y, transform.position.z);
+            transform.position = new Vector3(halfWayVector.x, halfWayVector.y + 5f, transform.position.z);
             float distance = Vector3.Distance(pos1, pos2);
             distance -= 20f;
             distance = Mathf.Max(distance, 0f); //Cannot be below 0
